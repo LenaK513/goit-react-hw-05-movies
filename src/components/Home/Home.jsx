791528e7c254cd0ab9d3fa90e6315a5194dc73d4
Olena <kryvtsovaolena@gmail.com>
2022-11-20 14:18:12 +0200

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import { getTrendingMovies } from 'api/fetchApi';
-import { AppBar } from 'components/AppBar/AppBar';
 import { Outlet } from 'react-router-dom';
 import { Title, MoviesList, MovieItem, NavItemStyled } from './Home.styled';
 
@@ -25,28 +24,3 @@ export const Home = () => {
     </div>
   );
 };
-
-// export const Home = () => {
-//   const [trendMovies, setTrendMovies] = useState([]);
-
-//   useEffect(() => {
-//     getTrendingMovies().then(data => setTrendMovies(data.results));
-//   }, []);
-
-//   return (
-//     <div>
-//       <Layout />
-//       <div>
-//         <Title>Trending Today</Title>
-//         <MoviesList>
-//           {trendMovies.map(({ id, title }) => (
-//             <MovieItem key={id}>
-//               <NavItemStyled>{title}</NavItemStyled>
-//             </MovieItem>
-//           ))}
-//         </MoviesList>
-//         <Outlet />
-//       </div>
-//     </div>
-//   );
-// };
